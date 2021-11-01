b37c450d691e87b422c8944e73c94896bf97b9d1
SergeySerzhan <s.sergey96@mail.ru>
2021-11-01 15:40:31 +0300

    fix: complete task 5-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const {readdir, open} = require('fs/promises');
+
+(async  function () {
+  const pathToDir = path.join(__dirname, 'styles');
+
+  const bundle = (await open(path.join(__dirname, 'project-dist', 'bundle.css'), 'w')).createWriteStream();
+
+  const files = await readdir(pathToDir, {withFileTypes: true});
+
+  for (const file of files) {
+    if(file.isFile() && path.extname(path.join(pathToDir, file.name)) === '.css') {
+      const fd = await open(path.join(pathToDir, file.name));
+      fd.createReadStream().pipe(bundle);
+    }
+  }
+})();
